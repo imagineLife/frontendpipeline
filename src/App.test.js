1c8a8bbdc7d7ecf0fb2f2a88ef16aff923bcd02d
Eric Laursen <ericjlaursen@gmail.com>
2022-05-02 15:35:02 -0400

    remove unused import, adjust text

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders horse text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const horseText = screen.getByText(/horse/i);
+  expect(horseText).toBeInTheDocument();
 });
