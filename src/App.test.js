081a7ac248b68ca810682101dc035f3a8ebc92de
Eric Laursen <ericjlaursen@gmail.com>
2022-05-02 16:42:37 -0400

    prettier

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders horse text', () => {
+test("renders horse text", () => {
   render(<App />);
   const horseText = screen.getByText(/horse/i);
   expect(horseText).toBeInTheDocument();
