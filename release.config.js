a88142f5e739c3be75caf7f933ca6b94a84a8775
Eric Laursen <ericjlaursen@gmail.com>
2022-05-02 19:30:02 -0400

    adjust branch name

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: "master",
+  branches: "main",
   repositoryUrl: "https://github.com/imagineLife/frontend-pipeline/",
   plugins: [
     "@semantic-release/commit-analyzer",
