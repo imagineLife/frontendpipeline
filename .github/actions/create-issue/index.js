5eeec32c5c9aa5e2bd466495c7b2ec6fa35e126f
Eric Laursen <ericjlaursen@gmail.com>
2022-05-07 11:52:05 -0400

    feat(new-action): introduce New Custom JS Action
    Here, an action is present to create a Github Issue. This action is also included in the
    "test-action" workflow.
    
    re #22

---
+++ .github/actions/create-issue/index.js
@@ -0,0 +1,31 @@
+// dependencies
+const core = require("@actions/core");
+const gh = require("@actions/github");
+
+try {
+  // capture inputs
+  const GH_TOKEN = core.getInput("gh_token");
+  const TITLE = core.getInput("title");
+  const BODY = core.getInput("body");
+  const ASSIGNEES = core.getInput("assignees");
+
+  // setup new issue obj
+  const NEW_ISSUE_OBJ = {
+    // pass entire obj
+    // includes owner
+    // includes repo
+    ...gh.context.repo,
+    title,
+    body,
+    assignees: ASSIGNEES ? ASSIGNEES.split("\n") : undefined
+  };
+
+  // do
+  const oktokit = new gh.GitHub(GH_TOKEN);
+  const { data } = oktokit.issues.create(NEW_ISSUE_OBJ);
+
+  // finish with some output
+  core.setOutput("created-issue", JSON.stringify(data));
+} catch (error) {
+  core.setFailed(error.message);
+}
